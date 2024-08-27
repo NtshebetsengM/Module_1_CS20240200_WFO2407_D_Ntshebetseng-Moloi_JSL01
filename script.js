caa4bb0fa1430241251d9093e95dcd03be484322
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-08-27 14:56:52 +0200

    completing validation function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
-
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+    const pattern = /^pet_\d{4}[A-Za-z]+$/;
+    
+    let result = pattern.test(input); // Placeholder for validation result
+if ( result === true){
+    document.getElementById('result').innerText = `Valid Syntax \u{1F7E2}`
+}else{
+    document.getElementById('result').innerText = `Invalid Syntax \u{1F534}`
+};
 }
 
 
